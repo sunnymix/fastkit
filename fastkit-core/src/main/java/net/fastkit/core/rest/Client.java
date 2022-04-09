b6cd9180c582a050d82191c68fd3a4facdacde84
sunnymix <sunny@sunnymix.com>
2022-04-09 22:43:49 +0800

    Client 添加 get 方法

--- fastkit-core/src/main/java/net/fastkit/core/rest/Client.java
+++ fastkit-core/src/main/java/net/fastkit/core/rest/Client.java
@@ -1,7 +1,50 @@
 package net.fastkit.core.rest;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import net.fastkit.core.common.Json;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 /**
  * @author sunnymix
  */
 public class Client {
+
+    private final String host;
+
+    private final HttpClient client;
+
+    private HttpClient _newClient() {
+        return HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
+    }
+
+    public Client(String host) {
+        this.host = host;
+        this.client = _newClient();
+    }
+
+    public <T> Response<T> get(String path) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(host + path))
+                .build();
+        try {
+            String responseString = client
+                    .send(request, HttpResponse.BodyHandlers.ofString())
+                    .body();
+            return Json.fromJson(responseString, new TypeReference<Response<T>>() {
+            });
+
+        } catch (Throwable ignored) {
+        }
+        return Response.error(0, "Internal server error");
+    }
+
 }
